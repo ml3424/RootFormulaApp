c9eac491eaacf8223711bf7fbad5dc1917ea20e2
ml3424 <ml3424@bs.amalnet.k12.il>
2024-04-06 16:38:13 +0300

    weddings!

--- app/src/main/java/com/example/rootformulaapp/MainActivity.java
+++ app/src/main/java/com/example/rootformulaapp/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.rootformulaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     EditText eTA, eTB, eTC;
+    Button btnA, btnB, btnC, btnRnd, btnCal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eTA = findViewById(R.id.eTA);
+        eTB = findViewById(R.id.eTB);
+        eTC = findViewById(R.id.eTC);
+
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnCal = findViewById(R.id.btnCal);
+        btnRnd = findViewById(R.id.btnRnd);
     }
 }
