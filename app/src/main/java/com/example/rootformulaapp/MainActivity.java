34d558707d83250a5e21af8dae5c22eeed6a6bc6
ml3424 <ml3424@bs.amalnet.k12.il>
2024-04-18 12:56:23 +0300

    workkkk

--- app/src/main/java/com/example/rootformulaapp/MainActivity.java
+++ app/src/main/java/com/example/rootformulaapp/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.rootformulaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int REQUEST_CODE = 7777;
     EditText eTA, eTB, eTC;
-    Button btnA, btnB, btnC, btnRnd, btnCal;
-    double a,b,c;
+    Button btnRnd, btnCal2;
+    TextView tV1;
+
+    double a,b,c = 0;
+    String result, inputA, inputB, inputC;
     Random rnd = new Random();
 
     @Override
@@ -25,38 +31,13 @@ public class MainActivity extends AppCompatActivity {
         eTA = findViewById(R.id.eTA);
         eTB = findViewById(R.id.eTB);
         eTC = findViewById(R.id.eTC);
+        tV1 = findViewById(R.id.textView76);
 
-        btnA = findViewById(R.id.btnA);
-        btnB = findViewById(R.id.btnB);
-        btnC = findViewById(R.id.btnC);
-        btnCal = findViewById(R.id.btnCal);
+        btnCal2 = findViewById(R.id.btnCal2);
         btnRnd = findViewById(R.id.btnRnd);
+
     }
 
-    public void clickA(View view)
-    {
-        String input = String.valueOf(eTA.getText());
-        if(checkInput(input))
-        {
-            a = Double.valueOf(input);
-        }
-    }
-
-    public void clickB(View view) {
-        String input = String.valueOf(eTB.getText());
-        if(checkInput(input))
-        {
-            b = Double.valueOf(input);
-        }
-    }
-
-    public void clickC(View view) {
-        String input = String.valueOf(eTC.getText());
-        if(checkInput(input))
-        {
-            c = Double.valueOf(input);
-        }
-    }
 
     public void clickRnd(View view) {
         a = Double.valueOf(rnd.nextInt(100) - 50);
@@ -66,9 +47,6 @@ public class MainActivity extends AppCompatActivity {
         eTA.setText(String.valueOf(a));
         eTB.setText(String.valueOf(b));
         eTC.setText(String.valueOf(c));
-    }
-
-    public void clickCal(View view) {
     }
 
 
@@ -89,13 +67,56 @@ public class MainActivity extends AppCompatActivity {
             }
             else
             {
-                if(input.equals("-.") || input.equals("+."))
+
+                if(input.charAt(1) >= '0' && input.charAt(1) <= '9')
                 {
-                    return false;
+                    return true;
+                }
+                else
+                {
+                    if(input.charAt(0) >= '0' && input.charAt(0) <= '9')
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
             }
         }
         return false;
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MainActivity.RESULT_OK) {
+                if (data != null) {
+                    result = data.getStringExtra("result");
+                    tV1.setText("last result was: \n" + result);
+                }
+            }
+        }
+    }
+
+    public void calResult(View view) {
+        inputA = String.valueOf(eTA.getText());
+        inputB = String.valueOf(eTB.getText());
+        inputC = String.valueOf(eTC.getText());
+
+        if(checkInput(inputA) && checkInput(inputB) && checkInput(inputC)) {
+            a = Double.valueOf(inputA);
+            b = Double.valueOf(inputB);
+            c = Double.valueOf(inputC);
+
+            Intent si = new Intent(this, ResultActivity.class);
+            si.putExtra("a", a);
+            si.putExtra("b", b);
+            si.putExtra("c", c);
+            startActivityForResult(si,REQUEST_CODE);
+        }
+    }
 }
--- app/src/main/java/com/example/rootformulaapp/ResultActivity.java
+++ app/src/main/java/com/example/rootformulaapp/ResultActivity.java
@@ -1,7 +1,11 @@
 package com.example.rootformulaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +13,11 @@ public class ResultActivity extends AppCompatActivity {
 
     ImageView iV;
     TextView resultText;
+    Button btnBack;
+    Intent gi;
+
+    double a,b,c;
+    String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +26,19 @@ public class ResultActivity extends AppCompatActivity {
 
         iV = findViewById(R.id.iV);
         resultText = findViewById(R.id.result);
+        btnBack = findViewById(R.id.btnBack);
+
+        gi = getIntent();
+        a = gi.getDoubleExtra("a",-888.0);
+        b = gi.getDoubleExtra("b",-888.0);
+        c = gi.getDoubleExtra("c",-888.0);
+        result = calResult(a,b,c);
 
 
     }
 
 
-    public void calResult(double a, double b, double c)
+    public String calResult(double a, double b, double c)
     {
         double inRoot = b*b - 4*a*c;
         if(inRoot > 0)
@@ -37,6 +53,13 @@ public class ResultActivity extends AppCompatActivity {
         {
             resultText.setText("No results");
         }
+        String returnText = resultText.getText().toString();
+        return returnText;
+    }
 
+    public void clickBack(View view) {
+        gi.putExtra("result",result);
+        setResult(RESULT_OK,gi);
+        finish();
     }
 }
